5725daf5b94ced50d0da7b5372ea9b1601baa8c6
Flusk <tpflusk@gmail.com>
2017-12-05 01:10:19 +0200

    Added video to scene

---
+++ Assets/Scripts/Flusk/Utility/IState.cs
@@ -0,0 +1,20 @@
+﻿namespace Flusk.Utility
+{
+    public interface IState
+    {
+        /// <summary>
+        /// First moment of state
+        /// </summary>
+        void Enter(IState previousState);
+
+        /// <summary>
+        /// State update
+        /// </summary>
+        void Tick();
+
+        /// <summary>
+        /// Last moment of state
+        /// </summary>
+        void Exit(IState nextState);
+    }
+}
---
+++ Assets/Scripts/StateBasedController/Example/States/Jump.cs
@@ -0,0 +1,23 @@
+﻿using Flusk.Utility;
+using UnityEngine;
+
+namespace StateBasedController.Example.States
+{
+    public class Jump : State
+    {
+        public Jump()
+        {
+            Debug.Log("This is the Jump State");
+        }
+        
+        public override void Enter(IState previousState)
+        {
+            Debug.Log("[Jump] Enter");
+        }
+        
+        public override void Exit(IState previousState)
+        {
+            Debug.Log("[Jump] Exit");
+        }
+    }
+}
