09ae3e8b1e004eb5e775280b0528d932917764fe
Flusk <tpflusk@gmail.com>
2017-12-19 01:32:20 +0200

    Added a video clip data asset for potential use

---
+++ Assets/Scripts/VideoTools/Experimental/VideoClipData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace VideoTools.Experimental
+{
+    [Serializable]
+    [CreateAssetMenu(fileName = "VideoClipData", menuName = "Video Clip Data", order = 0)]
+    public class VideoClipData : ScriptableObject, ISerializationCallbackReceiver
+    {
+        [SerializeField]
+        protected VideoClip videoClip;
+
+        public VideoClip VideoClip
+        {
+            get { return videoClip; }
+        }
+
+        [SerializeField, HideInInspector]
+        protected string userData;
+        public string UserData
+        {
+            get { return userData; }
+        }
+        
+        [SerializeField]
+        protected VideoClipBakedMethods bakedMethods;
+        public VideoClipBakedMethods BakedMethods
+        {
+            get { return bakedMethods; }
+        }
+
+        [SerializeField]
+        protected GameObject calllingPrefab;
+        public GameObject CalllingPrefab
+        {
+            get { return calllingPrefab; }
+        }
+
+        public void OnBeforeSerialize()
+        {
+            if (videoClip == null)
+            {
+                return;
+            }
+            VideoClipImporter importer = (VideoClipImporter) AssetImporter.GetAtPath(videoClip.originalPath);
+            userData = importer.userData;
+            bakedMethods = new VideoClipBakedMethods(videoClip, userData);
+            if (calllingPrefab != null)
+            {
+                bakedMethods.AssignEvents(calllingPrefab.GetComponentsInChildren<Component>());
+            }
+        }
+
+        public void OnAfterDeserialize()
+        {
+        }
+    }
+}
